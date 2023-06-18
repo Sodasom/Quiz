aba1b7b686213c16143ba549fbf524f76bedc3ea
Sodasom <1219dasom@gmail.com>
2023-06-19 01:02:40 +0900

    design: pathname에 따른 height 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Outlet, Router } from "react-router-dom";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 import LayoutContainer from "./components/LayoutContainer";
+import Loading from "./components/Loading";
 
 function App() {
   return (
-    <div className="flex justify-center before:w-full before:h-[320px] before:absolute before:top-0 before:left-0 before:bg-gradient-to-r from-teal-400 to-blue-900">
-      <LayoutContainer>
-        <Outlet />
-      </LayoutContainer>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="flex justify-center before:w-full before:h-[320px] before:absolute before:top-0 before:left-0 before:bg-gradient-to-r from-teal-400 to-blue-900">
+        <LayoutContainer>
+          <Outlet />
+        </LayoutContainer>
+      </div>
+    </Suspense>
   );
 }
 
--- src/components/LayoutContainer.tsx
+++ src/components/LayoutContainer.tsx
@@ -1,8 +1,14 @@
+import { useLocation } from "react-router-dom";
 import { LayoutProps } from "../types/props";
 
 export default function LayoutContainer({ children }: LayoutProps) {
+  const { pathname } = useLocation();
   return (
-    <main className="relative w-[680px] max-w-[680px] min-h-[480px] px-[20px] py-[40px] mt-[68px] mx-[20px] shadow-shadowBg rounded-[16px] bg-white md:p-[40px] md:mx-0">
+    <main
+      className={`relative w-[680px] max-w-[680px] min-h-[480px] px-[20px] py-[40px] mt-[68px] mx-[20px] shadow-shadowBg rounded-[16px] bg-white md:p-[40px] md:mx-0 ${
+        pathname === "/wrong" ? "min-h-[480px]" : "h-[400px]"
+      }`}
+    >
       {children}
     </main>
   );
